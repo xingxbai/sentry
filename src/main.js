a125be0a6ee406a8a6d91cb2a8ebe28f79ad8e53
xingxbai <38908638+xingxbai@users.noreply.github.com>
2020-06-16 14:52:04 +0800

    sentry地址

--- src/main.js
+++ src/main.js
@@ -10,12 +10,12 @@ import RavenVue from "raven-js/plugins/vue";
 
 Sentry.init({
   dsn:
-    "https://8c57b1916347437d81f210f0002ba8a2@o406377.ingest.sentry.io/5273727",
+    "https://673c5517d0444fb991706457939195db@o406377.ingest.sentry.io/5277878",
   integrations: [new VueIntegration({ Vue, attachProps: true })],
 });
 
 Raven.config(
-  "https://8c57b1916347437d81f210f0002ba8a2@o406377.ingest.sentry.io/5273727",
+  "https://673c5517d0444fb991706457939195db@o406377.ingest.sentry.io/5277878",
   {
     release: process.env.RELEASE_VERSION,
     debug: true,
